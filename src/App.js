a8cd12c3d0008635387e4d2c4d7b0bb8cf1f4b7b
Krona Emmanuel <kronaemmanuel@gmail.com>
2020-03-28 00:33:29 +0500

    Design started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 
-import Header from './components/Header'
-import Main from './components/Main'
+// import Main from './components/Main'
+
+import './App.css'
+import air_transmission from './static/images/air_transmission'
+import human_contact from './static/images/human_contact'
+import contaminated_objects from './static/images/contaminated_objects'
 
 function App() {
-  return(
-    <div>
-      <Header />
-      <Main />
+  return (
+    <div className="wrapper">
+      <div className="title area">
+        <h1>2019-nCoV</h1>
+        <h3>Wuhan Coronavirus</h3>
+      </div>
+      <div className="contagions area">
+        <img src={air_transmission}></img>
+        <img src={human_contact}></img>
+        <img src={contaminated_objects}></img>
+      </div>
+      <div className="symptoms area">Symptoms</div>
+      <div className="symptoms-img area">symptoms img</div>
+      <div className="chart area">Chart</div>
+      <div className="prevent area">Prevention</div>
     </div>
   )
 }
