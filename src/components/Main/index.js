157c24f9f4281a5e7c22a88fb9ee5c1bc3dd2b49
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-10-22 01:29:52 -0700

    Updated API

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -45,7 +45,7 @@ export default function Main() {
   const [countryData, setCountryData] = React.useState([])
   React.useEffect(() => {
     const fetchCountryData = async () => {
-      const response = await fetch('https://corona.lmao.ninja/countries?sort=country')
+      const response = await fetch('https://disease.sh/v3/covid-19/countries')
       const data = await response.json()
       let list = []
       data.forEach(item => {
@@ -70,7 +70,7 @@ export default function Main() {
   const [worldData, setWorldData] = React.useState({})
   React.useEffect(() => {
     const fetchWorldData = async () => {
-      const response = await fetch('https://corona.lmao.ninja/all')
+      const response = await fetch('https://disease.sh/v3/covid-19/all')
       const data = await response.json()
       setWorldData({
         cases: data.cases,
